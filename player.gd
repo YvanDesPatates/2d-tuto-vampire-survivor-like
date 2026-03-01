4c0c3e973abdf191c48e9b255806c0bc512fb846
yvan <rouxy@pop-os.numericable.fr>
2026-03-01 20:27:42 +0100

    player's decreasing health bar

--- player.gd
+++ player.gd
@@ -1,6 +1,10 @@
 extends CharacterBody2D
 
+signal health_reach_zero
+
+const DAMMAGE_RATE = 5.0 # max five damage per ennemies per second
 var speed: int = 600
+var health: float = 100.0
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -11,3 +15,10 @@ func _physics_process(delta: float) -> void:
 		%HappyBoo.play_walk_animation()
 	else :
 		%HappyBoo.play_idle_animation()
+	
+	var overlapping_enemies = %HitBox.get_overlapping_bodies()
+	if overlapping_enemies.size() > 0:
+		health -= DAMMAGE_RATE * overlapping_enemies.size() * delta
+		%HealthBar.value = health
+		if health <= 0.0:
+			health_reach_zero.emit()
