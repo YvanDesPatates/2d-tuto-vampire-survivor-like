44a9f547376faf0f5b29843e5cf6af2b8484f546
yvan <rouxy@pop-os.numericable.fr>
2026-03-01 19:02:07 +0100

    ennemies gets hitted and dies

--- enemy.gd
+++ enemy.gd
@@ -1,9 +1,25 @@
 extends CharacterBody2D
 
 @onready var player: Node2D = %Player
+@onready var smoke_explosion = preload("res://smoke_explosion/smoke_explosion.tscn")
 var speed: int = 300
+var health = 3
+
+func _ready() -> void:
+	%Slime.play_walk()
 
 func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * speed
 	move_and_slide()
+
+func take_damage():
+	health -= 1
+	%Slime.play_hurt()
+	
+	if health == 0:
+		queue_free()
+		
+		var smoke_instance:Node2D = smoke_explosion.instantiate()
+		get_parent().add_child(smoke_instance)
+		smoke_instance.global_position = global_position
