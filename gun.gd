c7b6cb6d392e406a8130e3ce8a4dc6b5b08c1f15
yvan <rouxy@nc-ass-vip.sdv.fr>
2026-03-01 21:33:32 +0100

    fix: gun does not shoot when no enemies in range

--- gun.gd
+++ gun.gd
@@ -1,13 +1,19 @@
 extends Area2D
 
+var is_enemy_in_range = false
+
 func _physics_process(delta: float) -> void:
 	var enemies_in_a_range: Array[Node2D] = get_overlapping_bodies()
-	if enemies_in_a_range.size() > 0:
+	is_enemy_in_range = enemies_in_a_range.size() > 0
+	if is_enemy_in_range == true:
 		var target_ennemy = enemies_in_a_range[0]
 		look_at(target_ennemy.global_position)
 
 
 func shoot():
+	if !is_enemy_in_range:
+		return
+	
 	const BULLET = preload("res://bullet.tscn")
 	var new_bullet: Node2D = BULLET.instantiate()
 	new_bullet.global_position = %BulletSpawn.global_position
