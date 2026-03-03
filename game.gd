1ac0dec6b2c965af17fb188ce5f5970147d39f83
yvan <yvis99@gmail.com>
2026-03-03 21:48:26 +0100

    spawn rate increase if you are too good

--- game.gd
+++ game.gd
@@ -1,13 +1,17 @@
 extends Node2D
 class_name Game
 
-const kill_distance_to_add_friend = 180
+const kill_distance_to_add_friend = 190
+
+var enemies_created = 0
+var enemies_killed = 0
 
 func spawn_enemy():
 	%PathFollow2D.progress_ratio = randf()
 	var enemy: Node2D = preload("uid://v45siqi72g4b").instantiate()
 	enemy.global_position = %PathFollow2D.global_position
 	add_child(enemy)
+	enemies_killed += 1
 
 
 func _on_spawn_ennemies_timer_timeout() -> void:
@@ -19,8 +23,17 @@ func _on_player_health_reach_zero() -> void:
 	get_tree().paused = true
 	
 func on_ennemy_died(ennemy: Node2D):
+	enemies_killed += 1
 	const LITTLE_FRIEND = preload("uid://dwib6q4b566dp")
 	if %Player.global_position.distance_to(ennemy.global_position) < kill_distance_to_add_friend:
 		var instance = LITTLE_FRIEND.instantiate()
 		instance.global_position = %Player.global_position - Vector2.DOWN
 		add_child(instance)
+
+
+func higher_spawn_rate_if_necessary():
+	if enemies_created < 10:
+		return
+	
+	if enemies_killed > enemies_created:
+		%SpawnEnnemiesTimer.wait_time -= 0.1
